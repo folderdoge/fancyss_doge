60fa61565b534af82184e5abaea5a5d3281e544f
FolderDoge <folderdoge@gmail.com>
2026-05-11 19:38:05 +0900

    build: regenerate version.json.js for 3.5.28-doge.8

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,6 +1,6 @@
 {
   "name": "fancyss",
   "version": "3.5.28-doge.8",
-  "md5_hnd_v8_full": "placeholder",
-  "md5_hnd_full": "placeholder"
+  "md5_hnd_v8_full": "9af59b969772f690fff87b40465cd3c1",
+  "md5_hnd_full": "ea4b9dc97c795062c610fc2fc45b32fd"
 }
